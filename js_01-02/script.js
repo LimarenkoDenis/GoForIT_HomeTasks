3a1486ac7ecd809379da48ee3c78fe16bce9e167
Limarenko Denis <limarenkodenis@gmail.com>
2016-02-23 00:48:25 +0200

    first js task complete

---
+++ js_01-02/script.js
@@ -0,0 +1,47 @@
+// ПЕРВАЯ ЧАСТЬ
+
+function pow(userInput, userPow) {
+  var result = 1;
+
+  for (var i = 0; i < userPow; i++) {
+    result *= userInput;
+  }
+
+  return result;
+}
+
+var userInput = prompt('Введите число','');
+var userPow = prompt('Введите степень','');
+
+if (userPow < 0) {
+  alert('Степень ' + userPow +
+    'не поддерживается, введите целую степень, большую 0');
+} else {
+  alert( pow(userInput, userPow) );
+}
+
+
+// ВТОРАЯ ЧАСТЬ
+
+var arrName = [];
+
+for(var x=0; x < 5; x++) {
+	var inputName = prompt('ВВедте имя','')
+	
+	arrName[x] = inputName;
+
+	console.log(arrName[x]);
+}
+
+var userName = prompt('Теперь введите свое имя','');
+ 
+function find(array) {
+	for (var i = 0; i < array.length; i++) {
+    	if (array[i] == userName) return true;
+  	}
+
+  	return false;
+}
+
+if(find(arrName) == true){alert(userName+" Вы успешно вошли")}
+else{alert('Ошибка')}	

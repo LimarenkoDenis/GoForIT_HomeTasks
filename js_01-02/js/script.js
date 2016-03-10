cfb1444f6b3c33d0f0185e3d984d8ee8ae01a135
Limarenko Denis <limarenkodenis@gmail.com>
2016-03-10 19:34:39 +0200

    correct style code js

--- js_01-02/js/script.js
+++ js_01-02/js/script.js
@@ -13,7 +13,7 @@ function pow(userInput, userPow) {
 var userInput = prompt('Введите число','');
 var userPow = prompt('Введите степень','');
 
-if (userPow < 0) {
+if ( userPow < 0 ) {
   console.log('Степень ' + userPow +
     'не поддерживается, введите целую степень, большую 0');
 } else {
@@ -25,23 +25,27 @@ if (userPow < 0) {
 
 var arrName = [];
 
-for(var x=0; x < 5; x++) {
+for(var x = 0; x < 5; x++) {
 	var inputName = prompt('ВВедте имя','')
 	
 	arrName[x] = inputName;
 
-	console.log(arrName[x]);
+	console.log( arrName[x] );
 }
 
 var userName = prompt('Теперь введите свое имя','');
  
 function find(array) {
 	for (var i = 0; i < array.length; i++) {
-    	if (array[i] == userName) return true;
-  	}
+     if ( array[i] == userName ) return true;
+  }
 
   	return false;
 }
 
-if(find(arrName) == true){alert(userName+" Вы успешно вошли")}
-else{alert('Ошибка')}	
+if( find(arrName) == true ){
+  alert(userName+" Вы успешно вошли")
+}
+else{
+  alert('Ошибка')
+}	

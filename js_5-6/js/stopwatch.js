7fadcda4dcb760d9411199b671cc76044ee8fd36
Limarenko Denis <limarenkodenis@gmail.com>
2016-04-01 03:49:51 +0300

    added 7-8 js homework, jQuery tab and hints

--- js_5-6/js/stopwatch.js
+++ js_5-6/js/stopwatch.js
@@ -4,7 +4,7 @@
 	var clear = document.querySelector('.clear');
 	var wrapper = document.querySelector('.wrapper');
 
-	timer.innerHTML = '0:0:000';
+	timer.innerHTML = '00:00:000';
 	start.innerHTML = 'Start';
 	clear.innerHTML = 'Clear';
 
@@ -31,7 +31,7 @@
 	function stop() {
 
 		clearInterval(id);
-		timer.innerHTML = '0:0:000';
+		timer.innerHTML = '00:00:000';
 		start.innerHTML = 'Start';
 		start.addEventListener('click', startTimer);
 		milliseconds = 0;
@@ -40,15 +40,12 @@
 
 		date = new Date(0, 0);
 
-
 		var block = document.querySelectorAll('.block');
-
 		for (var i = 0; i < block.length; i++) {
 			wrapper.removeChild(block[i])
 		};
 	};
 
-	var date;
 	var milliseconds = 0;
 	var seconds = 0;
 	var minutes = 0;
@@ -71,13 +68,21 @@
 		if (milliseconds >= 100) {
 			millisecondsTime = milliseconds;
 		}
+		if (milliseconds < 100) {
+			millisecondsTime = "0" + Number(milliseconds);
+		}
+		if (milliseconds < 10) {
+			millisecondsTime = "0" + "0" + Number(milliseconds);
+		}
 		if (seconds >= 60) {
 			seconds = 0;
 			++minutes;
 		}
-
-		if (minutes >= 60) {
-			minutes = 0;
+		if (seconds < 10) {
+			seconds = "0" + Number(seconds);
+		}
+		if (minutes < 10) {
+			minutes = "0" + Number(minutes);
 		}
 
 		++milliseconds;
@@ -92,5 +97,8 @@
 		wrapper.appendChild(block);
 
 		block.innerHTML = minutes + ':' + seconds + ':' + millisecondsTime;
+		if (timer.innerHTML == '00:00:000') {
+			block.innerHTML = '00:00:000';
+		}
 	}
 })();
---
+++ js_7-8/js/script.js
@@ -0,0 +1,63 @@
+$(function(){
+//forms hints
+   $('input').on('mouseenter', function(){
+   		$(this).siblings('.hint').animate({opacity: "show"}, "fast");
+   });
+   $('input').on('mouseleave', function(){
+   		$(this).siblings('.hint').animate({opacity: "hide"}, "fast");
+   });
+   $('.btnHelp').on('click',function(){
+   		$('.hint').show('fast');
+   });
+
+// tabs
+   $(".link").on('click', function () {
+        $(".link").removeClass("is-current");
+        $(this).addClass("is-current");        
+        $(".content").hide();
+        $($(this).attr("href")).show();
+        // var i = $(this).attr('href');
+        // $(i).show();
+   });
+});
+
+
+
+
+
+
+// это способ с visibility, opacity и transition. в  сss раскомитить
+// $(function(){
+//    $('input').on('mouseenter', function(){
+//    		$(this).siblings('.hint').addClass('show');
+//    });
+//    $('input').on('mouseleave', function(){
+//    		$(this).siblings('.hint').removeClass('show')
+//    });
+//    $('.btnHelp').on('click',function(){
+//    		$('.hint').addClass('show');
+//    })
+// });
+
+// остальные варианты решения
+
+// $(function(){
+//    $('input').hover(function(){
+//    		$(this).siblings('span').show('2000');
+//    },
+//    function () {
+//    		$(this).siblings('span').hide('1000');
+//    });
+// });
+
+// $(function(){
+//    $('input').on('mouseenter', function(){
+//    		$(this).siblings('span').show();
+//    });
+//    $('input').on('mouseleave', function(){
+//    		$(this).siblings('span').hide();
+//    });
+//    $('.btnHelp').on('click',function(){
+//    		$('.tip').show('fast');
+//    })
+// });

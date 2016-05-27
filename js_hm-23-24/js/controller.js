a1ecc0a603ad17b05b64808b249ae448f41806b7
Limarenko Denis <limarenkodenis@gmail.com>
2016-05-27 14:09:52 +0300

    mvc app with r.js

--- js_hm-23-24/js/controller.js
+++ js_hm-23-24/js/controller.js
@@ -1,28 +1,21 @@
-define(
-	'controller',
-	['jquery', 'model'],
-	function($){
-		function Controller(model, view) {
-			var self = this;
+function Controller(model, view) {
+    var self = this;
 
-			view.elements.addBtn.on('click', addItem);
-			view.elements.listContainer.on('click', '.item-delete', removeItem);
+    view.elements.addBtn.on('click', addItem);
+    view.elements.listContainer.on('click', '.item-delete', removeItem);
 
-			function addItem (){
-				var newItem = view.elements.input.val();
-				model.addItem(newItem);
-				view.renderList(model.data);
-				view.elements.input.val('');
-			};
+    function addItem() {
+        var newItem = view.elements.input.val();
+        model.addItem(newItem);
+        view.renderList(model.data);
+        view.elements.input.val('');
+    };
 
-			function removeItem(){
-				var item = $(this).attr('data-value');
-				model.removeItem(item);
-				view.renderList(model.data);
+    function removeItem() {
+        var item = $(this).attr('data-value');
+        model.removeItem(item);
+        view.renderList(model.data);
 
-			}
+    }
 
-		};
-		return Controller
-	}
-)	
+};
--- js_hm-23-24/js/model.js
+++ js_hm-23-24/js/model.js
@@ -1,38 +1,27 @@
-define(
-    'model',
+function Model(data) {
+    var self = this;
 
-    ['jquery'],
+    self.data = data;
 
-    function($) {
-        return {
-            Model: function(data) {
-                var self = this;
-
-                self.data = data;
-
-                self.addItem = function(item) {
-                    if (item.length === 0) {
-                        return;
-                    }
-
-                    self.data.push(item);
-
-                    return self.data;
-                };
-
-                self.removeItem = function(item) {
-                    var index = self.data.indexOf(item);
-
-                    if (index === -1) {
-                        return;
-                    };
-
-                    self.data.splice(index, 1);
-
-                    return self.data;
-                };
-            }
+    self.addItem = function(item) {
+        if (item.length === 0) {
+            return;
         }
 
-    }
-)
+        self.data.push(item);
+
+        return self.data;
+    };
+
+    self.removeItem = function(item) {
+        var index = self.data.indexOf(item);
+
+        if (index === -1) {
+            return;
+        };
+
+        self.data.splice(index, 1);
+
+        return self.data;
+    };
+}
